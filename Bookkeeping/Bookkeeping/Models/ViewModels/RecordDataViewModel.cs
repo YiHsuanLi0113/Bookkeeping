3869a0ab68edd804815c62828a79f6409c4401de
YiHsuanLi0113 <ysl19950113@gmail.com>
2017-08-13 21:34:39 +0800

    新增ViewModel

---
+++ Bookkeeping/Bookkeeping/Models/ViewModels/RecordDataViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Bookkeeping.Models.ViewModels
+{
+    public class RecordDataViewModel
+    {
+        public string RecordClass { get; set; }
+        public DateTime RecordDate { get; set; }
+        public int RecordAmount { get; set; }
+    }
+}
