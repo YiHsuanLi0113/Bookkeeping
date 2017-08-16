48093c00c26f47354eb61d7bf2985283776fd7c1
YiHsuanLi0113 <ysl19950113@gmail.com>
2017-08-16 22:32:12 +0800

    加入VeiwModel

--- Bookkeeping/Bookkeeping/Models/ViewModels/RecordDataViewModel.cs
+++ Bookkeeping/Bookkeeping/Models/ViewModels/RecordDataViewModel.cs
@@ -7,6 +7,7 @@ namespace Bookkeeping.Models.ViewModels
 {
     public class RecordDataViewModel
     {
+        public int RecordId { get; set; }
         public string RecordClass { get; set; }
         public DateTime RecordDate { get; set; }
         public int RecordAmount { get; set; }
