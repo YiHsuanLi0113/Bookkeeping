97fe6c45fc2221b0e0dc07dff19927bc982d612e
YiHsuanLi0113 <ysl19950113@gmail.com>
2017-08-17 23:32:46 +0800

    改為[Display(Name = string)]

--- Bookkeeping/Bookkeeping/Models/ViewModels/RecordDataViewModel.cs
+++ Bookkeeping/Bookkeeping/Models/ViewModels/RecordDataViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,8 +9,14 @@ namespace Bookkeeping.Models.ViewModels
     public class RecordDataViewModel
     {
         public int RecordId { get; set; }
+
+        [Display(Name = "類別")]
         public string RecordClass { get; set; }
+
+        [Display(Name = "日期")]
         public DateTime RecordDate { get; set; }
+
+        [Display(Name = "金額")]
         public int RecordAmount { get; set; }
     }
 }
