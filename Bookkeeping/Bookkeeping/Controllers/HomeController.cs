904ad0f86cd0277e1fda6a8acafd600680bb2fa1
YiHsuanLi0113 <ysl19950113@gmail.com>
2017-08-17 23:34:20 +0800

    加入PagedList.Mvc

--- Bookkeeping/Bookkeeping/Controllers/HomeController.cs
+++ Bookkeeping/Bookkeeping/Controllers/HomeController.cs
@@ -4,27 +4,44 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Bookkeeping.Models.ViewModels;
+using Bookkeeping.Repositories;
+using Bookkeeping.Models.Service;
+using PagedList;
 
 namespace Bookkeeping.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        private readonly RecordListService _recordSvc;
+        private int pageSize = 10;
+
+        public HomeController()
         {
-            List<RecordDataViewModel> record = new List<RecordDataViewModel>();
-            DateTime date = new DateTime(2017, 7, 24);
+            var unitOfWork = new EFUnitOfWork();
+            _recordSvc = new RecordListService(unitOfWork);
+        }
 
-            for(var i = 0; i < 10; i++)
-            {
-                record.Add(new RecordDataViewModel()
-                {
-                    RecordClass = "支出" ,
-                    RecordDate = date,
-                    RecordAmount = 1000
-                });
-            }               
+        public ActionResult Index(int page = 1)
+        {
+            //List<RecordDataViewModel> record = new List<RecordDataViewModel>();
+            //DateTime date = new DateTime(2017, 7, 24);
 
-            return View(record);
+            //for(var i = 0; i < 10; i++)
+            //{
+            //    record.Add(new RecordDataViewModel()
+            //    {
+            //        RecordClass = "支出" ,
+            //        RecordDate = date,
+            //        RecordAmount = 1000
+            //    });
+            //}               
+            int currentPage = page < 1 ? 1 : page;
+
+            var source = _recordSvc.Lookup();
+
+            var result = source.ToPagedList(currentPage, pageSize);
+
+            return View(result);
         }
 
         public ActionResult About()
@@ -33,6 +50,36 @@ namespace Bookkeeping.Controllers
 
             return View();
         }
+
+        // GET: RecordList/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: RecordList/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "RecordId,RecordClass,RecordAmount,RecordDate")] RecordDataViewModel record)
+        {
+            if (ModelState.IsValid)
+            {
+                record.RecordId = Math.Abs((Guid.NewGuid().GetHashCode()));
+                _recordSvc.Add(record);
+                _recordSvc.Save();
+
+                return RedirectToAction("Index");
+            }
+            var result = new RecordDataViewModel()
+            {
+                RecordId = record.RecordId,
+                RecordClass = record.RecordClass,
+                RecordAmount = record.RecordAmount,
+                RecordDate = record.RecordDate,
+            };
+            return View(result);
+        }
+
 
         public ActionResult Contact()
         {
