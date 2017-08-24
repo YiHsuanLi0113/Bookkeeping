9ab4768779ce852f82c9f0b0d6dbfa7fa5096761
YiHsuanLi0113 <ysl19950113@gmail.com>
2017-08-24 22:39:02 +0800

    model validate

--- Bookkeeping/Bookkeeping/Models/ViewModels/RecordDataViewModel.cs
+++ Bookkeeping/Bookkeeping/Models/ViewModels/RecordDataViewModel.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using Bookkeeping.ValidateAttribute;
 
 namespace Bookkeeping.Models.ViewModels
 {
@@ -11,15 +12,23 @@ namespace Bookkeeping.Models.ViewModels
     {
         public int RecordId { get; set; }
 
+        [Required(ErrorMessage = "請選擇{0}")]
         [DisplayName("類別")]
         public string RecordClass { get; set; }
 
+        [Required(ErrorMessage = "請選擇{0}")]
+        [BeforeToday(ErrorMessage = "選擇之{0}不得超過今日")]
         [DisplayName("日期")]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}")]
         public DateTime RecordDate { get; set; }
 
+        [Required(ErrorMessage = "請輸入{0}")]
         [DisplayName("金額")]
+        [RegularExpression(@"^\+?[1-9][0-9]*$", ErrorMessage = "請輸入大於0、整數之金額")]
         public int RecordAmount { get; set; }
 
+        [Required(ErrorMessage = "紀錄一下自己把錢花在哪吧")]
+        [StringLength(100, ErrorMessage = "輸入之內容長度不可超過{1}字")]
         [DisplayName("備註")]
         public string RecordMemo { get; set; }
     }
---
+++ Bookkeeping/Bookkeeping/ValidateAttribute/BeforeTodayAttribute.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Bookkeeping.ValidateAttribute
+{
+    public sealed class BeforeTodayAttribute : ValidationAttribute
+    {
+        public BeforeTodayAttribute()
+        {
+        }
+        public override bool IsValid(object date)
+        {
+            DateTime today = DateTime.Now.Date;
+            DateTime pickDate = ((DateTime)date).Date;
+
+            var isValidate = pickDate<= today;
+            if (date == null)
+                return true;
+            if (isValidate)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
+
+
